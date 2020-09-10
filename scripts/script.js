b9f505c8c96af82db9ff646c7e8b8200c52f31b1
pedrohenriquebraga <gamesantos7@gmail.com>
2020-09-10 11:17:26 -0300

    Cards with red border are musics with explicit lyric

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@
 let searchInput = document.querySelector('#searchInput')
 searchButton = document.querySelector('#search span')
 musics = document.querySelector('.musics')
-cards = document.querySelectorAll('.card')
 
 slider = document.querySelector('.slider')
 previousCard = document.querySelector('#previous')
@@ -21,7 +20,6 @@ previousCard.addEventListener('click', () => {
 })
 
 nextCard.addEventListener('click', () => {
-    console.log(window.getComputedStyle(slider).width)
     maxScroll = parseInt(window.getComputedStyle(slider).width.replace('px', ''))
     if (scrollValue < maxScroll) {
         scrollValue += 340
@@ -34,31 +32,40 @@ nextCard.addEventListener('click', () => {
 
 // Acessa a API e procura o que foi pesquisado
 async function getInfoCards(search) {
-    let response = {}
+    let response
     await axios.get(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${search}`).then(results => {
-        console.log(results)
-        return response = results.data['data']
+        if (results) {
+            return response = results.data['data']
+        }
     }).catch(err => {
         return console.error(err)
     })
-    return response
+    if (response) {
+        return response
+    }
 }
 
 
 // Renderiza o cartão no slide
 function renderCard(infoCard) {
-    musics.innerHTML += `
-        <div class="card">
-            <img src="${infoCard.album.cover_medium}" alt="Música de ${infoCard.artist.name}" loading="lazy">
-            <p class="singer">${infoCard.artist.name}</p>
-            <p class="name_music">${infoCard.title_short}</p>
-            <audio src="${infoCard.preview}" controls type="audio/*"></audio>
-            <a href="${infoCard.link}" target="_blank" rel="noopener noreferrer">Ouça Agora <i class="fa fa-play"></i></a>
-        </div>
-        `
-
-    if (infoCard.explicit_lyrics) {
-        cards[cards.length - 1].style.border = '3px solid #f00'
+    if (infoCard) {
+        musics.innerHTML += `
+            <div class="card">
+                <img src="${infoCard.album.cover_medium}" alt="Música de ${infoCard.artist.name}" loading="lazy">
+                <p class="singer">${infoCard.artist.name}</p>
+                <p class="name_music">${infoCard.title_short}</p>
+                <audio src="${infoCard.preview}" controls type="audio/*"></audio>
+                <a href="${infoCard.link}" target="_blank" rel="noopener noreferrer">Ouça Agora <i class="fa fa-play"></i></a>
+            <span class="explicit hidden">Explícito</span>
+            </div>
+            `
+        if (infoCard.explicit_lyrics) {
+            let cards = document.querySelectorAll('.card')
+            cards[cards.length - 1].style.border = '3px solid #f00'
+            cards[cards.length - 1].innerHTML += '<span class="explicit">Letra Explícita</span>'
+        }
+    } else {
+        alert('Erro ao realizar a pesquisa!!')
     }
 }
 
